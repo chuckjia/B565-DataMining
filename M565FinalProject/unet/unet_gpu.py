c0207b0eb22512eb7f7d05d0923a03cc8ec64454
Chuck Jia <chuckjia@users.noreply.github.com>
2018-04-27 16:52:01 -0400

    Regular update

--- M565FinalProject/unet/unet_gpu.py
+++ M565FinalProject/unet/unet_gpu.py
@@ -43,8 +43,8 @@ keras.backend.set_session(sess)
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 IMG_CHANNELS = 3
-TRAIN_PATH = '/home/iu_ai_club/Documents/chuckjia/datamining/data/stage1_train/'
-TEST_PATH = '/home/iu_ai_club/Documents/chuckjia/datamining/data/stage1_test/'
+TRAIN_PATH = '/Users/chuckjia/Documents/Workspace/DataStorage/B565/stage1_train/'
+TEST_PATH = '/Users/chuckjia/Documents/Workspace/DataStorage/B565/stage1_test/'
 
 warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
 seed = 42
@@ -159,7 +159,7 @@ model.summary()
 
 
 # Fit model
-numEpoch = 10;
+numEpoch = 2;
 earlystopper = EarlyStopping(patience=5, verbose=1)
 checkpointer = ModelCheckpoint('model-dsbowl2018-1.h5', verbose=1, save_best_only=True)
 results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=8, epochs=numEpoch, 
@@ -195,4 +195,45 @@ for imgNo in range(len(X_test_id)):
     imageio.imsave(filename, 255 * np.squeeze(preds_test_t[imgNo]))
 
 
+
+
+
+
+
+# Run-length encoding stolen from https://www.kaggle.com/rakhlin/fast-run-length-encoding-python
+def rle_encoding(x):
+    dots = np.where(x.T.flatten() == 1)[0]
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b + 1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
+
+def prob_to_rles(x, cutoff=0.5):
+    lab_img = label(x > cutoff)
+    for i in range(1, lab_img.max() + 1):
+        yield rle_encoding(lab_img == i)
+        
+        
+new_test_ids = []
+rles = []
+for n, id_ in enumerate(test_ids):
+    rle = list(prob_to_rles(preds_test_upsampled[n]))
+    rles.extend(rle)
+    new_test_ids.extend([id_] * len(rle))
+    
+    
+# Create submission DataFrame
+sub = pd.DataFrame()
+sub['ImageId'] = new_test_ids
+sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
+sub.to_csv('sub-dsbowl2018-1.csv', index=False)
+    
+    
+    
+    
+    
+    
 
